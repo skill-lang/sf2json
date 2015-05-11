ea6201bbc2e27b0d2a25d7412660088f186d8925
Timm Felden <feldentm@informatik.uni-stuttgart.de>
2015-05-11 15:00:53 +0200

    added entry point

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+/**
+ * @author Timm Felden
+ */
+public class Main {
+
+    /**
+     * Converts field data of an sf-file into an equivalent json-file
+     * 
+     * @param args
+     *            in-path out-path
+     */
+    public static void main(String[] args) {
+        // TODO [[using skill v1.0]]
+
+    }
+
+}
